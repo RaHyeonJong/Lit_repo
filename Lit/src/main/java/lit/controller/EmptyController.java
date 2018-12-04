5dbb30fc25342b9baf63eeb38fe2962f87f7b2eb
dong <dong@LAPTOP-P15CRCHL>
2018-12-04 14:35:55 +0900

    11

--- Lit/src/main/java/lit/controller/EmptyController.java
+++ Lit/src/main/java/lit/controller/EmptyController.java
@@ -4,11 +4,11 @@ public class EmptyController {
 
 	public static void main(String[] args) {
 		System.out.println("이즈리얼");
-<<<<<<< HEAD
+
 		System.out.println("탐켄치");
-=======
+
 		System.out.println("레오나");
 
->>>>>>> branch 'master' of https://github.com/RaHyeonJong/Lit_repo.git
+		System.out.println("트리스타나");
 	}
 }
