3c00d5a8cf9531d7bee9cf2ebdcaed6e2de94c7a
raracraft <raracraft@DESKTOP-6NNE3K9>
2018-12-04 15:04:49 +0900

    ra

--- Lit/src/main/java/lit/dao/face/LoginDao.java
+++ Lit/src/main/java/lit/dao/face/LoginDao.java
@@ -1,5 +1,9 @@
 package lit.dao.face;
 
+import lit.dto.Member;
+
 public interface LoginDao {
+
+	void login(Member member);
 
 }
--- Lit/src/main/java/lit/service/face/LoginService.java
+++ Lit/src/main/java/lit/service/face/LoginService.java
@@ -1,5 +1,9 @@
 package lit.service.face;
 
+import lit.dto.Member;
+
 public interface LoginService {
+
+	boolean login(Member member);
 
 }
