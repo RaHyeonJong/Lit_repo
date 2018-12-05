4f11de4ceaedee33eefed624666dfb9a4c895aa0
raracraft <raracraft@DESKTOP-6NNE3K9>
2018-12-05 16:32:33 +0900

    ra

--- Lit/src/main/java/lit/dto/Lodge.java
+++ Lit/src/main/java/lit/dto/Lodge.java
@@ -1,5 +1,23 @@
 package lit.dto;
 
-public class Lodge {
+import java.util.Date;
 
+public class Lodge {
+	private int lodge_no;
+	private int building_case_no;
+	private String building_case;
+	private int lodge_case_no;
+	private String lodge_case;
+	private String lodge_name;
+	private int lodge_capacity;
+	private int lodge_room;
+	private String lodge_addr;
+	private String convenient_facility;
+	private String convenient_area;
+	private int ready_time;
+	private String check_in_time;
+	private int available_term;
+	private int stay_cost;
+	private Date regit_date;
+	private int lodge_activation;
 }
