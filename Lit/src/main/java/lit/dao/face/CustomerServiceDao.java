b9f02ebe6ec4ffea812a0d3b9cf6a5354da1c4f4
FonziLee <intlastrow@naver.com>
2018-12-05 13:44:04 +0900

    이성훈 오후꺼

--- Lit/src/main/java/lit/dao/face/CustomerServiceDao.java
+++ Lit/src/main/java/lit/dao/face/CustomerServiceDao.java
@@ -2,14 +2,9 @@ package lit.dao.face;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import lit.dto.Member;
 
-
-=======
 import lit.dto.Board;
-import lit.dto.Member;
->>>>>>> branch 'master' of https://github.com/RaHyeonJong/Lit_repo.git
 
 public interface CustomerServiceDao {
 
@@ -18,11 +13,7 @@ public interface CustomerServiceDao {
 	public void writer(Member member);
 
 	//-문의내용 리스트
-<<<<<<< HEAD
-	
-=======
 
->>>>>>> branch 'master' of https://github.com/RaHyeonJong/Lit_repo.git
 	public List<Board> list();
 
 	//-리스트에서 버튼 누르면 나오는 것
