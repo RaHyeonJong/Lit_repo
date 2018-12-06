654d8d0a8c5daa7bd52070f6426330b61f396cd9
hsk61 <hsk61@LAPTOP-UNHSKT2M>
2018-12-06 13:43:37 +0900

    18.12.6 hsk

--- Lit/src/main/java/lit/service/face/HostService.java
+++ Lit/src/main/java/lit/service/face/HostService.java
@@ -2,4 +2,28 @@ package lit.service.face;
 
 public interface HostService {
 
+	
+	//-----------단계별 정보등록
+	//1단계숙소정보등록
+	public void insertFirst();
+
+	//2단계숙소정보등록
+	public void insertSecond();
+
+	//3단계숙소정보등록
+	public void insertThird();
+
+	
+	//---------단계별 정보수정
+	
+	//1단계 정보수정
+	public void hostElementFirstFix();
+
+	
+	//2단계 정보수정
+	public void hostElementSecondFix();
+
+	//3단계 정보수정
+	public void hostElementThirdFix();
+
 }
