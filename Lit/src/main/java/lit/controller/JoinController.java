fe3a8bfa157b329c293c530586281173697872db
user1 <user1@user1-PC>
2018-12-06 10:35:05 +0900

    ra

--- Lit/src/main/java/lit/controller/JoinController.java
+++ Lit/src/main/java/lit/controller/JoinController.java
@@ -1,24 +1,45 @@
 package lit.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import lit.dto.Member;
+import lit.service.face.JoinService;
 
 @Controller
 public class JoinController {
 	
+	@Autowired JoinService joinService;
+	
 	@RequestMapping(value="/join", method=RequestMethod.GET)
-	public void join() {}
+	public String join() {
+		return "join/join";
+	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String joinProcess() {
+	public String joinProcess(Member member) {
+		joinService.insertMember(member);
 		
-		
-		return null;
+		return "join/result";
 	}
 	
 	@RequestMapping(value="/join/checkId")
-	public void checkId() {
+	public ModelAndView checkId(Member member) {
+		ModelAndView mav = new ModelAndView();
+		boolean existId = joinService.checkId(member);
 		
+		Map<String, Boolean> map = new HashMap<>();
+		map.put("existId", existId);
+		
+		mav.addObject("map", map);
+		mav.setViewName("jsonView");
+		
+		return mav;
 	}
 }
---
+++ Lit/src/main/java/lit/controller/MypageController.java
@@ -0,0 +1,10 @@
+package lit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MypageController {
+	
+
+
+}
