90fda807591b75a2e3a3d510a20649911de43783
raracraft <raracraft@DESKTOP-6NNE3K9>
2018-12-11 19:47:49 +0900

    ra

--- Lit/src/main/java/lit/service/impl/LoginServiceImpl.java
+++ Lit/src/main/java/lit/service/impl/LoginServiceImpl.java
@@ -1,10 +1,11 @@
 package lit.service.impl;
 
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import lit.dao.face.LoginDao;
@@ -66,17 +67,35 @@ public class LoginServiceImpl implements LoginService{
 	public void sendMail(Member member) {
 		String from = "LifeIsTrip@lit.com";
 		String to = member.getMem_id();
-		String title = "[Life Is Trip] 비밀번호 재설정 메일입니다.";
-		String content = "비밀번호 재설정 링크 주소?mem_id="+member.getMem_id();
+		String title = "[LIFE IS TRIP] 비밀번호 재설정 링크메일 입니다.";
+		String content = "<div style=\"width:600px; height:600px;\">\r\n" + 
+				"<table style=\"width:100%;\">\r\n" + 
+				"<tr><td style=\"font-size:40px; font-weight: bold;\">\r\n" + 
+				"<img style=\"height: 38px;\" \r\n" + 
+				"	src=\"https://mblogthumb-phinf.pstatic.net/20120807_173/wldnjs980227_1344341038774YQ23Y_JPEG/%B9%D0%C2%A4%B8%F0%C0%DA_%C7%D8%C0%FB%B4%DC.jpg?type=w2\"/>\r\n" + 
+				"LIFE IS TRIP</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:15px; font-size:18px;\">"+ member.getMem_name() +" 님, 안녕하세요.</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:15px; font-size:18px;\">비밀번호 재설정을 요청하셨습니다.</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:15px; font-size:18px;\">회원님이 요청하지 않은 경우 아무런 조치를 취하실 필요가 없습니다.<br>\r\n" + 
+				"요청하셨다면 다음 링크를 이용해 비밀번호를 재설정하세요.</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:30px;\">\r\n" + 
+				"<a style=\"text-decoration:none;\" href=\"localhost:8585/login/changePw?mem_id="+ member.getMem_id() +"\">\r\n" + 
+				"<div style=\"display:table; width:200px; height:50px; text-align:center; background-color:#FF5A5F; border-radius:3px;\">\r\n" + 
+				"<div style=\"display:table-cell; vertical-align:middle; color:white; font-size: 18px; cursor:pointer;\">비밀번호 재설정하기</div>\r\n" + 
+				"</div></a>\r\n" + 
+				"</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:15px; font-size:18px;\">감사합니다.</td></tr>\r\n" + 
+				"<tr><td style=\"padding-top:5px; font-size:18px;\">라이프이즈트립 팀 드림</td></tr>\r\n" + 
+				"</table>\r\n" + 
+				"</div>";
 		
 		try {
 			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
 		 
-			messageHelper.setFrom(from);  // 보내는사람 생략하거나 하면 정상작동을 안함
-			messageHelper.setTo(to);     // 받는사람 이메일
-			messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
-			messageHelper.setText(content);  // 메일 내용
+			message.setFrom(new InternetAddress(from));  // 보내는사람 생략하거나 하면 정상작동을 안함
+			message.addRecipient(RecipientType.TO, new InternetAddress(to));     // 받는사람 이메일
+			message.setSubject(title); // 메일제목은 생략이 가능하다
+			message.setText(content, "UTF-8", "html");  // 메일 내용
 		     
 			mailSender.send(message);
 		} catch(Exception e){
