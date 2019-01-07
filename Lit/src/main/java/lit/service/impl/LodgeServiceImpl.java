739bd13e94c22f21550723d4ee6be9c60a56936a
hyun2 <hyun2@DESKTOP-AGKMLO0>
2019-01-07 20:49:13 +0900

    eeeeee

--- Lit/src/main/java/lit/service/impl/LodgeServiceImpl.java
+++ Lit/src/main/java/lit/service/impl/LodgeServiceImpl.java
@@ -42,9 +42,9 @@ public class LodgeServiceImpl implements LodgeService {
 	}
 
 	@Override
-	public List<Image> LodgeImage() {
+	public List<Image> LodgeImage(Lodge lodge) {
 		
-		return lodgedao.SelectLodgeImage();
+		return lodgedao.SelectLodgeImage(lodge);
 	}
 
 	@Override
@@ -88,9 +88,9 @@ public class LodgeServiceImpl implements LodgeService {
 	
 	
 	@Override
-	public List<Comment> commentList() {
+	public List<Comment> commentList(Lodge lodge) {
 		// TODO Auto-generated method stub
-		return lodgedao.lodgeComment();
+		return lodgedao.lodgeComment(lodge);
 	}
 
 	
